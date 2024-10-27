6067d2c0ff9319cfaa9f2449d8700f7d58673d7b
sagar-chauhan1 <sagar.chauhan.coding@gmail.com>
2024-10-27 22:36:27 +0530

    Factorial of numbar

---
+++ factorial_of_num.java
@@ -0,0 +1,16 @@
+public class factorial_of_num {
+    public static int calcfactorial(int n){
+        if (n==1 || n==0) {
+            return 1;
+        }
+    
+    int fact_nm1=calcfactorial(n-1);
+    int fact_n = n*fact_nm1;
+    return fact_n;
+    }
+    public static void main(String args[]){
+        int n=5;
+        int ans=calcfactorial(n);
+        System.out.print(ans);
+    }
+}
