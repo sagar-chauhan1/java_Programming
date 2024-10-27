e94fcdf6601fcca291a068d7a63c6703c96e4df3
sagar-chauhan1 <sagar.chauhan.coding@gmail.com>
2024-10-27 16:11:25 +0530

    recursion print numbers

---
+++ recursion.java
@@ -0,0 +1,29 @@
+// public class recursion {
+//     public static void printNum(int n){
+//         if(n==0){
+//             return;
+//         }
+//         System.out.println(n);
+//         printNum(n-1);
+//     }
+//     public static void main(String args[]) {
+//         int n=5;
+//         printNum(n);
+//     }
+// }
+
+
+public class recursion {
+    public static void printNum(int n){
+        if(n==6){
+            return;
+        }
+        System.out.println(n);
+        printNum(n+1);
+    }
+
+    public static void main(String args[]){
+        int n=1;
+        printNum(n);
+    }
+}
