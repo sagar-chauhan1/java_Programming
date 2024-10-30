2f0ff91f414cd907ca62d800c42a531726975ba5
sagar-chauhan1 <sagar.chauhan.coding@gmail.com>
2024-10-30 16:00:35 +0530

    object file using class file

---
+++ Sagar_class.java
@@ -0,0 +1,12 @@
+public class Sagar_class {
+    private int x;
+    private int y;
+    public void setData(int p, int q){
+        x=p;
+        y=q;
+    }
+    public void display(){
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
---
+++ Sagar_object.java
@@ -0,0 +1,9 @@
+public class Sagar_object {
+    public static void main(String args[]){
+        int p=5;
+        int q=8;
+        Sagar_class e1 = new Sagar_class();
+        e1.setData(p, q);
+        e1.display();
+    }
+}
